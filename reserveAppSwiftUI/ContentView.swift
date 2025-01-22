f81b0f3590b7719f6bd5dc70abf68f03ef55b13b
Ivan Terekhov <terekhovvan@yahoo.com>
2025-01-22 14:09:15 +0200

    initial

---
+++ reserveAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  reserveAppSwiftUI
+//
+//  Created by Иван Терехов on 22.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ reserveAppSwiftUI/reserveAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  reserveAppSwiftUIApp.swift
+//  reserveAppSwiftUI
+//
+//  Created by Иван Терехов on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct reserveAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
